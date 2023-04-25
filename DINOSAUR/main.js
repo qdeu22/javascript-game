c72c111c0e17d62b11e5b0b0eaa5381294fc191c
qdeu22 <qdeu22@gmail.com>
2023-04-25 15:58:32 +0900

    feat: 기본적인 초록색 사각형으를 표현해보기

--- DINOSAUR/main.js
+++ DINOSAUR/main.js
@@ -3,3 +3,6 @@ var ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
+
+ctx.fillStyle = "green"; //초록색 네모
+ctx.fillRect(10, 10, 100, 100); // 10,10 좌표에 100x100크기 생성
