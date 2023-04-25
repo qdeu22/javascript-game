76856635b96885211201009e7617103981d005a1
qdeu22 <qdeu22@gmail.com>
2023-04-25 15:56:49 +0900

    fix: height를 Height로 수정.

--- DINOSAUR/main.js
+++ DINOSAUR/main.js
@@ -2,4 +2,4 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth - 100;
-canvas.height = window.innerheight - 100;
+canvas.height = window.innerHeight - 100;
