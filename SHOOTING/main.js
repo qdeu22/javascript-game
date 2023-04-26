2a9b3ae0e7035dadcae42dd2da57273001e97fc4
qdeu22 <qdeu22@gmail.com>
2023-04-26 09:42:18 +0900

    feat: 개임 이미지 불러오기

--- SHOOTING/main.js
+++ SHOOTING/main.js
@@ -10,3 +10,29 @@ canvas.height = 700;
 
 //이제 HTML에 넣기
 document.body.appendChild(canvas);
+
+var backgroundImage;
+var spaceshipImage;
+var bulletImage;
+var enemyImage;
+var gameOverImage;
+
+/**
+ * 게임 이미지 불러오기
+ */
+function loadImage() {
+  backgroundImage = new Image();
+  backgroundImage.src = "images/background.jpg";
+
+  spaceshipImage = new Image();
+  spaceshipImage.src = "images/spaceship.png";
+
+  bulletImage = new Image();
+  bulletImage.src = "images/bullet.png";
+
+  enemyImage = new Image();
+  enemyImage.src = "images/enemy.png";
+
+  gameOverImage = new Image();
+  gameOverImage.src = "images/gameOver.png";
+}
