a60a46558f49eaef39ef9ab49b87244b31d83955
qdeu22 <qdeu22@gmail.com>
2023-04-26 20:48:51 +0900

    feat: 캔버스 초기 셋팅!

---
+++ BRICK/main.js
@@ -0,0 +1,9 @@
+var canvas = document.createElement("canvas");
+var context = canvas.getContext("2d");
+
+canvas.id = "myCanvas";
+canvas.width = 400;
+canvas.height = 700;
+
+//이제 HTML에 넣기
+document.body.appendChild(canvas);
