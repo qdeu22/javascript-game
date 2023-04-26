a5f4f19c9af684c1be8196843b8389b57d115413
qdeu22 <qdeu22@gmail.com>
2023-04-26 20:53:52 +0900

    feat: 공 생성

--- BRICK/main.js
+++ BRICK/main.js
@@ -7,3 +7,9 @@ canvas.height = 700;
 
 //이제 HTML에 넣기
 document.body.appendChild(canvas);
+
+context.beginPath();
+context.arc(50, 50, 10, 0, Math.PI * 2);
+context.fillStyle = "#0095DD";
+context.fill();
+context.closePath();
