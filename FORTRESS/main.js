9895da571cd58d4d4fa99b22ccf6159195cb9708
qdeu22 <qdeu22@gmail.com>
2023-04-28 01:29:28 +0900

    init: 포트리스 초기 셋팅

---
+++ FORTRESS/main.js
@@ -0,0 +1,8 @@
+var canvas = document.createElement("canvas");
+var context = canvas.getContext("2d");
+
+canvas.width = 1000;
+canvas.height = 700;
+
+//이제 HTML에 넣기
+document.body.appendChild(canvas);
