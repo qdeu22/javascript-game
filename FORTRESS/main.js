5224bbaf9e3185e4a03a3c6c12842848ce612c23
qdeu22 <qdeu22@gmail.com>
2023-04-28 01:52:27 +0900

    feat: 좌, 우 키입력 반응!

--- FORTRESS/main.js
+++ FORTRESS/main.js
@@ -4,13 +4,36 @@ var context = canvas.getContext("2d");
 canvas.width = 1000;
 canvas.height = 700;
 
+//이제 HTML에 넣기
+document.body.appendChild(canvas);
+
 var tankWidth = 50;
 var tankHeight = 50;
 var tankX = 0;
 var tankSpeed = 3;
 
-//이제 HTML에 넣기
-document.body.appendChild(canvas);
+var tankLeftPressed = false;
+var tankRightPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key === "ArrowLeft") {
+    tankLeftPressed = true;
+  }
+  if (e.key === "ArrowRight") {
+    tankRightPressed = true;
+  }
+}
+function keyUpHandler(e) {
+  if (e.key === "ArrowLeft") {
+    tankLeftPressed = false;
+  }
+  if (e.key === "ArrowRight") {
+    tankRightPressed = false;
+  }
+}
 
 function drawTank() {
   context.lineWidth = 5; // 선 굵기 5
