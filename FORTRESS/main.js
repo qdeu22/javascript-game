cbe37516b487655d3626cc3f69833ae4b77f22ea
qdeu22 <qdeu22@gmail.com>
2023-04-28 02:09:55 +0900

    feat: 탱크에 캐논 달기

--- FORTRESS/main.js
+++ FORTRESS/main.js
@@ -15,6 +15,12 @@ var tankSpeed = 3;
 var tankLeftPressed = false;
 var tankRightPressed = false;
 
+var tankCenterX; // 탱크의 중심 x좌표
+var tankCenterY; // 탱크의 중심 y좌표
+var cannonAngle = Math.PI / 4; // 캐논의 각도 (45도)
+var cannonAngleDIF = Math.PI / 60; // 각도의 변화량
+var cannonLength = tankWidth * Math.sqrt(2); // 캐논의 길이
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
@@ -44,12 +50,23 @@ function drawTank() {
   context.lineTo(tankX + tankWidth, canvas.height); // 탱크의 오른쪽 하단 모서리
   context.lineTo(tankX, canvas.height); // 탱크의 왼쪽 하단 모서리
   context.lineTo(tankX, canvas.height - tankHeight); // 탱크의 왼쪽 상단 모서리
+
+  context.moveTo(tankCenterX, tankCenterY);
+  context.lineTo(
+    tankCenterX + cannonLength * Math.cos(cannonAngle),
+    tankCenterY - cannonLength * Math.sin(cannonAngle)
+  );
+
   context.stroke(); // 경로를 따라 선을 그림
   context.closePath();
 }
 
 function main() {
   context.clearRect(0, 0, canvas.width, canvas.height);
+
+  tankCenterX = tankX + 0.5 * tankWidth;
+  tankCenterY = canvas.height - 0.5 * tankHeight;
+
   if (tankLeftPressed && tankX > 0) {
     tankX -= tankSpeed;
   }
